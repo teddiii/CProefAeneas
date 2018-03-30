c3d06aaa376da0e6e24d7c5654e562d52b9047a8
Aeneas Brutseart <aeneas_brutseart@LIVE.BE>
2018-03-30 21:18:32 +0200

    Add project files.

---
+++ CproefLib/AppDbContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CproefLib
+{
+    class AppDbContext
+    {
+    }
+
+    public AppDbContext() : base(@"Data Source=LAPTOP-FAC37MEN\SQLEXPRESS;Initial Catalog=CProef;Persist Security Info=True;User ID=cproef;Password=bryan")
+    {
+
+    }
+}
